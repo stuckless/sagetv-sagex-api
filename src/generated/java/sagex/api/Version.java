82050871a8b07df275693e81dcd7cbc4a37e78d5
Sean Stuckless <sean.stuckless@gmail.com>
2016-08-19 21:21:25 -0400

    Added API Rule to fix the GetFavoritePerson return type

--- src/generated/java/sagex/api/Version.java
+++ src/generated/java/sagex/api/Version.java
@@ -1,6 +1,6 @@
 package sagex.api;
 public class Version {
-    public static final String VERSION = "9.0.0.6";
+    public static final String VERSION = "9.0.0.7";
     public static String GetVersion() {
        return VERSION;
     }
