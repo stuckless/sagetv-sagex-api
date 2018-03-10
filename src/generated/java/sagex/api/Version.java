8c90f50481e0d6c357012b512ddcdfb75cd5ebae
Sean Stuckless <sean.stuckless@gmail.com>
2018-03-10 16:32:08 -0500

    version update

--- src/generated/java/sagex/api/Version.java
+++ src/generated/java/sagex/api/Version.java
@@ -1,7 +1,7 @@
 
 package sagex.api;
 public class Version {
-    public static final String VERSION = "9.0.0.8";
+    public static final String VERSION = "9.1.7.0";
     public static String GetVersion() {
        return VERSION;
     }
